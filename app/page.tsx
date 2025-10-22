60767a59f372f0e380f567bfacb2e1a03af033c7
Peixotim <pedropeixotovz@gmail.com>
2025-10-22 11:19:42 -0300

    feat: Modifica a pagina `page.tsx`

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Index from "./index";
 
-const queryClient = new QueryClient();
-
 export default function HomePage() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-      
-        <Sonner />
-        <Index />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
+  return <Index />;
 }
