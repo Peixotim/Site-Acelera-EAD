20cb0b2775e4d8eca11dbafb56b3382e83d1b0ef
Peixotim <pedropeixotovz@gmail.com>
2025-10-22 17:08:23 -0300

    Feat: Adiciona o pixel do google

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Poppins } from "next/font/google";
 
-
 import "@/styles/globals.css";
-
-
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -24,48 +21,44 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-
 export const metadata: Metadata = {
-
   title: {
     default: "Acelera EAD | Cursos Online para o Topo do Sucesso",
     template: "%s | Acelera EAD",
   },
 
-
-  description: "A Acelera EAD oferece cursos tecnológicos e especializações focados em acelerar sua carreira. Estude online e alcance seus objetivos profissionais conosco.",
-
+  description:
+    "A Acelera EAD oferece cursos tecnológicos e especializações focados em acelerar sua carreira. Estude online e alcance seus objetivos profissionais conosco.",
 
   openGraph: {
     title: "Acelera EAD | Impulsione Sua Carreira",
-    description: "Cursos 100% online na Acelera EAD para você que busca crescimento e sucesso profissional.",
-    url: 'https://aceleraead.com.br', // Sugestão de URL
-    siteName: 'Acelera EAD',
+    description:
+      "Cursos 100% online na Acelera EAD para você que busca crescimento e sucesso profissional.",
+    url: "https://aceleraead.com.br", // Sugestão de URL
+    siteName: "Acelera EAD",
     images: [
       {
-        url: 'https://aceleraead.com.br/images/og-image-default.jpg', // Lembre-se de criar e hospedar esta imagem
+        url: "https://aceleraead.com.br/images/og-image-default.jpg", // Lembre-se de criar e hospedar esta imagem
         width: 1200,
         height: 630,
-        alt: 'Logo da Acelera EAD e Símbolo de Aceleração Profissional',
+        alt: "Logo da Acelera EAD e Símbolo de Aceleração Profissional",
       },
     ],
-    locale: 'pt_BR',
-    type: 'website',
+    locale: "pt_BR",
+    type: "website",
   },
-
 
   twitter: {
-    card: 'summary_large_image',
-    title: 'Acelera EAD: Sua Carreira em Alta Velocidade',
-    description: 'Descubra como os cursos da Acelera EAD podem impulsionar sua jornada profissional.',
+    card: "summary_large_image",
+    title: "Acelera EAD: Sua Carreira em Alta Velocidade",
+    description:
+      "Descubra como os cursos da Acelera EAD podem impulsionar sua jornada profissional.",
   },
-
 
   icons: {
-    icon: '/favicon.ico',
+    icon: "/favicon.ico",
   },
 };
-
 
 export default function RootLayout({
   children,
@@ -73,14 +66,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-
     <html lang="pt-br">
-      <body
-
-        className={`${poppins.variable} antialiased scroll-smooth`}
-      >
+      <meta
+        name="google-site-verification"
+        content="6aiavGRkxbAjXlDMk-Ai6fhDxcuMq0o6lp9vr2x3Q1k"
+      />
+      <body className={`${poppins.variable} antialiased scroll-smooth`}>
         {children}
-      
       </body>
     </html>
   );
